26288134d9076fcde11ad6e9c42d3829e9d21ef1
Priyanshu Mishra <172821911+priyanshu936932@users.noreply.github.com>
2026-01-31 14:26:59 +0000

    Math learning in javascript

--- Basics/Lecture1.js
+++ Basics/Lecture1.js
@@ -1,3 +1,33 @@
-console.log("Welcome to Lecture 1 of the Basics module!");
+// console.log("Welcome to Lecture 1 of the Basics module!");
 
-console.log("Priyanshu Mishra 2026")
+// console.log("Priyanshu Mishra 2026")
+
+// console.log(Math);
+// console.log(Math.abs(-323))
+// console.log(Math.round(4.623))
+// console.log(Math.ceil(4.323))
+// console.log(Math.floor(4.2324))
+// console.log(Math.min(2,3,4,5,6,7,8,9,-23,-45,-67))
+// console.log(Math.max(2,3,4,5,6,7,8,9,-23,-45,-67))
+console.log(Math.random())
+
+console.log((Math.random()*10)+1)
+
+const min=10;
+const max=20;
+Math.random()*(max-min)+min;
+
+console.log(Math.floor(Math.random()*(max-min))+min);
+
+console.log(Math.sin(Math.PI/2))
+// Math.PI gives the value of PI
+console.log(Math.cos(0))
+console.log(Math.tan(Math.PI/4))
+console.log(Math.hypot(3,4))
+
+console.log(Math.log(1))
+
+
+
+
+
