be4801d8268d36b48e6835c55faf26d3d08b1505
Priyanshu Mishra <172821911+priyanshu936932@users.noreply.github.com>
2026-01-31 20:35:53 +0000

    API Touch

---
+++ Objects/Lecture3.js
@@ -0,0 +1,25 @@
+const course={
+    courseName:"Learning JavaScript",
+    price:999,
+    courseInstructor:"Hitesh Choudhary",
+    courseContent:["JavaScript Basics","DOM Manipulation","ES6 Features","Asynchronous JavaScript"],
+}
+
+const {courseInstructor:instructor}=course;
+// console.log(courseInstructor); // Output: Hitesh Choudhary
+
+console.log(instructor); // Output: Hitesh Choudhary
+
+//API to work with objects
+
+// {
+//     "name": "Alice",
+//     "age": 30,
+//     "isStudent": false,
+// }
+
+[
+    {},
+    {},
+    {}
+]
